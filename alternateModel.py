e3528da2b829cc80e765d14baee1dc79976f05bf
debraj__m <debrajm2204@gmail.com>
2025-05-25 22:57:07 +0530

    updated path

--- alternateModel.py
+++ alternateModel.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+model = YOLO(r"C:\Users\debra\Desktop\Dataset\yolov8n.pt")
 
 model.train(
-    data=r"C:\Users\debra\Desktop\Dataset\weld_quality_dataset\data.yaml",
+    data=r"C:\Users\debra\Desktop\Dataset\Drive link Dataset\driveLink.yaml",
     epochs=10,
     imgsz=640,
     batch=8
