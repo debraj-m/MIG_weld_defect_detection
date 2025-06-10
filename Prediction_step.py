61e72684d4ead4cfaa617c56c3e2b43058ffdeb3
debraj__m <debrajm2204@gmail.com>
2025-06-10 12:04:25 +0530

    Refactor code structure for improved readability and maintainability

--- Prediction_step.py
+++ Prediction_step.py
@@ -76,5 +76,5 @@ def detect_and_process(image_path):
 
 
 if __name__ == "__main__":
-    image_path = r"C:\Users\debra\Desktop\CODE\Dataset\Pictures from welding\PXL_20250525_100534824.MP.jpg"
+    image_path = r"C:\Users\debra\Desktop\CODE\Dataset\Pictures from welding\PXL_20250525_104231099.jpg"
     detect_and_process(image_path)
